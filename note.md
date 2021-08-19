0a5b4f999acd7c92b5177ed1752c3613fd5ee262
lvafi <l_vafi@yahoo.com>
2021-08-19 14:16:57 -0700

    Add question show page

---
+++ note.md
@@ -0,0 +1,10 @@
+create react app:
+1 - nvm install 14.15.4
+2 - nvm use 14.15.4
+3 - npx create-react-app awesome_answers
+4 - delete unnecessary stuff:
+    App.js -> get rid of everything inside a header
+    App.js -> remove import logo from './logo.svg';
+    App.test.js
+    logo.svg 
+5 - npm start 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const QuestionShowPage = () => {
+  return(
+    <main>
+      <QuestionDetails/>
+      <AnswerDetails/>
+    </main>
+  )
+}
+
+const QuestionDetails = () => {
+  return (
+    <div>
+      <h2>What is your favourite color?</h2>
+      <p> Green, Blue, Magenta, etc.</p>
+      <p>By John Snow</p>
+      <p>
+        <small>seen 10 times</small>
+        <small>last edited 2 hours</small>
+      </p>
+    </div>
+  )
+}
+
+const AnswerDetails = () => {
+  return (
+    <div>
+      <p>This is my answer</p>
+      <p>By Steve Jobs</p>
+      <p>
+        <strong>Created at</strong> 1 day ago
+      </p>
+    </div>
+  )
+}
+
+
+// each react component can only return one react Element (div)
+
+// React.StrictMode is a development only component will give you hints about the components that on writing
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<React.StrictMode>
+    //<App />,   
+  //</React.StrictMode>,
+  <QuestionShowPage />,
+  
   document.getElementById('root')
 );
 
